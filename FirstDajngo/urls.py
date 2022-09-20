7dc3181af24fec666e2b61cd1a8d186dd28ec8ba
boo <eyurchenko@specialist.ru>
2022-09-20 19:24:35 +0300

    +about

--- FirstDajngo/urls.py
+++ FirstDajngo/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from MainApp.views import main_page
+from MainApp import views
 
 urlpatterns = [
-    path('', main_page),
+    path('', views.main_page),
+    path('about', views.about),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def main_page(request):
     return HttpResponse("Hello-)")
+
+
+def about(request):
+    author = {
+        "name": "Евгений",
+        "surname": "Юрченко",
+        "email": "eyurchenko@specialist.ru"
+    }
+    return HttpResponse(str(author))
