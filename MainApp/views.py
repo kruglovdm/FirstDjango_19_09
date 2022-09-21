82f04a184b163c49a155494fd5ce2b555e754253
kruglovdm <110811400+kruglovdm@users.noreply.github.com>
2022-09-21 19:14:30 +0300

    Update views.py

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,46 +1,51 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+# Create your views here.
+def main_page(request):
+    context = autor
+    return render(request, 'index.html', context)
 
-author = {
-    "name": "Евгений",
-    "surname": "Юрченко",
-    "phone": "8-923-600-01-02",
-    "email": "eyurchenko@specialist.ru"
+def about_page(request):
+    context = autor
+    return render(request, 'about.html', context)
+
+def item_page(request, item_id: int):
+    item = next((x for x in items if x["id"] == item_id), None)
+    context = {'item': item}
+    if item:
+        return render(request, 'item.html', context)
+
+    html_practice = f'Товар с id={item_id} не найден'
+    raise Http404(html_practice)
+
+def itemslist_page(request):
+    context = {
+        'items': items
+    }
+    return render(request, 'items_list.html', context)
+
+h1 = '"Изучаем Django"'
+#autor_name_surname = 'Круглов Д.М.'
+#autor_name = 'Денис'
+#autor_patronymic = 'Михайлович'
+#autor_surname = 'Круглов'
+#autor_fhone = '8-903-232-08-02'
+#autor_email = 'kruglovdm@mail.ru'
+
+autor = {
+   'name_surname': 'Круглов Д.М.',
+   'name': 'Денис',
+   'patronymic': 'Михайлович',
+   'surname': 'Круглов',
+   'phone': '8-903-232-08-02',
+    'email': 'kruglovdm@mail.ru'
 }
 
 items = [
-    {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
-    {"id": 20, "name": "Куртка кожаная", "quantity": 2},
-    {"id": 3, "name": "Coca-cola 1 литр", "quantity": 12},
-    {"id": 5, "name": "Кепка", "quantity": 124},
-    {"id": 15, "name": "!!!!", "quantity": 124},
+    {'id': 1, 'name': 'Кроссовки abibas', 'quantity': 5},
+    {'id': 2, 'name': 'куртка кожанная', 'quantity': 2},
+    {'id': 3, 'name': 'Coca-cola 1 литр', 'quantity': 12},
+    {'id': 4, 'name': 'Картофель фри', 'quantity': 0},
+    {'id': 5, 'name': 'Кепка', 'quantity': 124},
+    {'id': 6, 'name': 'Ремень', 'quantity': 1},
 ]
-
-
-def main_page(request):
-    return render(request, 'index.html')
-
-
-def about(request):
-    result = f"""Имя: <b>{author['name']}</b> <br>
-                Фамилия: <b>{author['surname']}</b> <br>
-                телефон: <b>{author['phone']}</b> <br>
-                email: <b>{author['email']}</b> <br>
-    """
-    return HttpResponse(result)
-
-
-def item(request, item_id: int):
-    for item in items:
-        if item["id"] == item_id:
-            item_result = f"Товар: {item['name']}, количество: {item['quantity']}"
-            return HttpResponse(item_result)
-    # return HttpResponseNotFound(f"Item with id={item_id} not found")
-    raise Http404(f"Item with id={item_id} not found")
-
-
-def items_list(request):
-    context = {
-        "items": items
-    }
-    return render(request, 'items_list.html', context)
